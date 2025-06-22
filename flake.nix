7439f99394f565f76ef7034c11bafe5d4eac6611
Ignacio Rodriguez <ignacionr@gmail.com>
2025-06-22 18:46:26 -0300

    Add valid Nix flake for reproducible C++/CMake/SDL2 builds

--- flake.nix
+++ flake.nix
@@ -1,60 +1,58 @@
 {
-  "description": "Rouen project flake for reproducible C++/CMake/SDL2 development and CI",
-  "inputs": {
-    "nixpkgs": {
-      "url": "github:NixOS/nixpkgs/nixos-24.05"
-    },
-    "flake-utils": {
-      "url": "github:numtide/flake-utils"
-    }
-  },
-  "outputs": {
-    "self",
-    "nixpkgs",
-    "flake-utils",
-    "...": "{ self, nixpkgs, flake-utils, ... }@inputs: flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = import nixpkgs { inherit system; };
-      cmake = pkgs.cmake;
-      ninja = pkgs.ninja;
-      sdl2 = pkgs.sdl2;
-      pkg-config = pkgs.pkg-config;
-      curl = pkgs.curl;
-      openssl = pkgs.openssl;
-      # Add more dependencies as needed
-      buildInputs = [ cmake ninja sdl2 pkg-config curl openssl ];
-    in {
-      devShells.default = pkgs.mkShell {
-        buildInputs = buildInputs;
-        shellHook = "export CC=${pkgs.gcc}/bin/gcc; export CXX=${pkgs.gcc}/bin/g++";
-      };
-      packages.default = pkgs.stdenv.mkDerivation {
-        name = "rouen";
-        src = ./.;
-        nativeBuildInputs = [ cmake ninja pkg-config ];
-        buildInputs = [ sdl2 curl openssl ];
-        cmakeFlags = [ "-DCMAKE_BUILD_TYPE=Release" ];
-        installPhase = ''
-          mkdir -p $out/bin
-          cp build-nix/rouen $out/bin/
-        '';
-      };
-      checks.default = pkgs.stdenv.mkDerivation {
-        name = "rouen-tests";
-        src = ./.;
-        nativeBuildInputs = [ cmake ninja pkg-config ];
-        buildInputs = [ sdl2 curl openssl ];
-        cmakeFlags = [ "-DCMAKE_BUILD_TYPE=Debug" ];
-        buildPhase = ''
-          mkdir -p build-tests
-          cd build-tests
-          cmake ../tests -DCMAKE_BUILD_TYPE=Debug
-          cmake --build . --parallel
-        '';
-        checkPhase = ''
-          cd build-tests
-          ctest --output-on-failure
-        '';
-      };
-    })"
-  }
+  description = "Rouen project flake for reproducible C++/CMake/SDL2 development and CI";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in {
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            pkgs.cmake
+            pkgs.ninja
+            pkgs.sdl2
+            pkgs.pkg-config
+            pkgs.curl
+            pkgs.openssl
+            pkgs.gcc
+          ];
+        };
+
+        packages.default = pkgs.stdenv.mkDerivation {
+          pname = "rouen";
+          version = "0.1.0";
+          src = ./.;
+          nativeBuildInputs = [ pkgs.cmake pkgs.ninja pkgs.pkg-config ];
+          buildInputs = [ pkgs.sdl2 pkgs.curl pkgs.openssl ];
+          cmakeFlags = [ "-DCMAKE_BUILD_TYPE=Release" ];
+          installPhase = ''
+            mkdir -p $out/bin
+            cp build-nix/rouen $out/bin/
+          '';
+        };
+
+        checks.default = pkgs.stdenv.mkDerivation {
+          pname = "rouen-tests";
+          version = "0.1.0";
+          src = ./.;
+          nativeBuildInputs = [ pkgs.cmake pkgs.ninja pkgs.pkg-config ];
+          buildInputs = [ pkgs.sdl2 pkgs.curl pkgs.openssl ];
+          cmakeFlags = [ "-DCMAKE_BUILD_TYPE=Debug" ];
+          buildPhase = ''
+            mkdir -p build-tests
+            cd build-tests
+            cmake ../tests -DCMAKE_BUILD_TYPE=Debug
+            cmake --build . --parallel
+          '';
+          checkPhase = ''
+            cd build-tests
+            ctest --output-on-failure
+          '';
+        };
+      });
 }
