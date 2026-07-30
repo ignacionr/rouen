206e5d99776e2321cb91b89bde9a43a1b19a4cb7
Ignacio Rodriguez <ignacionr@gmail.com>
2026-07-30 01:09:15 -0300

    Provide cross-platform inline fallbacks for mac helper functions

--- src/helpers/mac_menu_helper.hpp
+++ src/helpers/mac_menu_helper.hpp
@@ -3,7 +3,13 @@
 struct SDL_Window;
 
 namespace rouen::platform {
+#if defined(__APPLE__)
     void disable_mac_cmd_w_menu_item();
     int get_mac_titlebar_height(SDL_Window* window);
     float get_mac_backing_scale_factor(SDL_Window* window);
+#else
+    inline void disable_mac_cmd_w_menu_item() {}
+    inline int get_mac_titlebar_height(SDL_Window* /*window*/) { return 0; }
+    inline float get_mac_backing_scale_factor(SDL_Window* /*window*/) { return 1.0f; }
+#endif
 }
--- src/helpers/mac_mic_permissions.h
+++ src/helpers/mac_mic_permissions.h
@@ -1,5 +1,9 @@
 #pragma once
 
 namespace rouen::platform {
+#if defined(__APPLE__)
     void request_mac_microphone_permission();
+#else
+    inline void request_mac_microphone_permission() {}
+#endif
 }
