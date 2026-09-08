b65c1fa8396bef4f91f8a613feee7868ee97f9be
Ignacio Rodriguez <ignacionr@gmail.com>
2026-09-08 09:20:36 -0300

    fix(build): add -Wno-tu-local to target compile options in cmake/warnings.cmake for GCC 15

--- cmake/warnings.cmake
+++ cmake/warnings.cmake
@@ -256,6 +256,8 @@ elseif(CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
     -fpermissive
     -Wno-subobject-linkage
     $<$<VERSION_GREATER_EQUAL:${CMAKE_CXX_COMPILER_VERSION},15.0>:-Wno-template-names-tu-local>
+    $<$<VERSION_GREATER_EQUAL:${CMAKE_CXX_COMPILER_VERSION},15.0>:-Wno-tu-local>
+    $<$<VERSION_GREATER_EQUAL:${CMAKE_CXX_COMPILER_VERSION},15.0>:-Wno-error=tu-local>
   )
   
   # Add function for GCC too
@@ -272,6 +274,8 @@ elseif(CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
       # This is a known GCC 14 regression; disable the warning to avoid build log spam.
       $<$<VERSION_GREATER_EQUAL:${CMAKE_CXX_COMPILER_VERSION},14.0>:-Wno-null-dereference>
       $<$<VERSION_GREATER_EQUAL:${CMAKE_CXX_COMPILER_VERSION},15.0>:-Wno-template-names-tu-local>
+      $<$<VERSION_GREATER_EQUAL:${CMAKE_CXX_COMPILER_VERSION},15.0>:-Wno-tu-local>
+      $<$<VERSION_GREATER_EQUAL:${CMAKE_CXX_COMPILER_VERSION},15.0>:-Wno-error=tu-local>
       -Wno-subobject-linkage
       -Wno-pedantic
       -fpermissive
