8316c1cf367f83f4e4595bb47dc0d151d037e1ab
Ignacio Rodriguez <ignacionr@gmail.com>
2026-09-09 13:25:53 -0300

    fix(debug): remove GNU attribute modifier preceding template declaration in debug::format_log to fix GCC 15 module parsing

--- src/helpers/debug.hpp
+++ src/helpers/debug.hpp
@@ -166,6 +166,10 @@ namespace debug {
         return ss.str();
     }
 
+    inline std::string format_log_v(std::string_view fmt, std::format_args args) {
+        return std::vformat(fmt, args);
+    }
+
     template<typename T>
     inline decltype(auto) format_clean(const T& val) {
         if constexpr (std::is_same_v<T, std::string> || std::is_same_v<T, std::string_view>) {
@@ -175,9 +179,6 @@ namespace debug {
         }
     }
 
-#if defined(__GNUC__) && !defined(__clang__)
-    __attribute__((noinline))
-#endif
     template<typename... Args>
     inline std::string format_log(std::string_view fmt, const Args&... args) {
         if constexpr (sizeof...(Args) == 0) {
@@ -185,7 +186,7 @@ namespace debug {
         } else {
             auto args_tuple = std::make_tuple(format_clean(args)...);
             return std::apply([fmt](const auto&... lvalue_args) -> std::string {
-                return std::vformat(fmt, std::make_format_args(lvalue_args...));
+                return format_log_v(fmt, std::make_format_args(lvalue_args...));
             }, args_tuple);
         }
     }
